38a19a04bcbab63e9da6e1a7c4c3dfac2e9fbdd2
mgoncalves3 <55608718+mgoncalves3@users.noreply.github.com>
2022-03-19 01:23:23 +0000

    Grid creation done. Paintable done

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-let input = undefined;
 const wrapper = document.getElementById("wrapper-grid");
 const changeGridBtn = document.getElementById("gridBtn");
 changeGridBtn.addEventListener("click", () => {
 	input = prompt("Enter your grid size:");
-	console.log(input)
+	createGrid(input);
 });
 
+let resetGrid = () => {
+	while (wrapper.firstChild) {
+		wrapper.removeChild(wrapper.firstChild);
+	}	
+};
+
+// Makes cells change color when hovering
+function makeSketch () {
+	wrapper.childNodes.forEach(cell => cell.addEventListener("mouseover", () => {cell.style.backgroundColor = "black"}))
+};
+
 function createGrid(val) {
-	let cellSize = (wrapper.scrollHeight / val) - 4;
-	console.log(cellSize)
+	// Start by reseting the current grid
+	resetGrid();
+
+	// Calculate the new cell size
+	let cellSize = (wrapper.scrollHeight / val);
+	
 	for (let i = 0, totalCells = val * val; i < totalCells; i++) {
 
+		// Create a new div and set it's attributes
 		let newDiv = document.createElement("div");
 		newDiv.classList.add("cell");
 
@@ -19,6 +34,9 @@ function createGrid(val) {
 
 		wrapper.appendChild(newDiv);
 	}
+
+	// After creating the grid, make it usable 
+	makeSketch();
 }
 
-createGrid(input);
+createGrid(16); // Default state
