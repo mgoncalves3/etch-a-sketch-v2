934c879d86150f918141f2d72aac5fc7d793b7cd
mgoncalves3 <marco.g@sapo.pt>
2022-03-21 17:30:47 +0000

    Features finished. To do: styling

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
+// Selectors
 const wrapper = document.getElementById("wrapper-grid");
 const changeGridBtn = document.getElementById("gridBtn");
+const clearBtn = document.getElementById("clearBtn");
+const radioButtons = document.querySelectorAll("input");
 
-changeGridBtn.addEventListener("click", () => {
-	input = prompt("Enter your grid size:");
-  
-	while (input < 1 || input > 100) {
-	  input = prompt("The grid must be between 1 and 100 squares wide");
-  }
-  createGrid(input);
-});
+// Helper function definitions
 
 let resetGrid = () => {
+  // Keep looking for child nodes (cells) and remove them one at a time
   while (wrapper.firstChild) {
     wrapper.removeChild(wrapper.firstChild);
   }
 };
 
-function selectColor () {
-	let radioButtons = document.get(`input`);
-	
-	radioButtons.forEach( btn => {
-		console.log(btn)
-	})
+// Select every cell in the grid and reset it
+let clearGrid = () => {
+  let cells = document.getElementsByClassName("cell");
+  for (let cell of cells) {
+    cell.style.backgroundColor = "white";
+  }
+};
+
+// Get a random number between 0-255 to use as RGB value
+function getRandomColor() {
+  return Math.floor(Math.random() * 256);
 }
 
-// Makes cells change color when hovering
-function makeSketch() {
-  wrapper.childNodes.forEach( cell =>
-    cell.addEventListener("mouseover", () => {
-      cell.style.backgroundColor = "black";
-    })
-  );
+/* Black color is checked by default so we only need to check
+if the 'random' button is on */
+function selectColor() {
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked === true) {
+      if (radioButtons[i].value === "random") {
+        return `rgb(${getRandomColor()},${getRandomColor()},${getRandomColor()})`;
+      }
+      return "black";
+    }
+  }
 }
 
 function createGrid(val) {
@@ -48,6 +54,7 @@ function createGrid(val) {
     newDiv.style.height = `${cellSize}px`;
     newDiv.style.width = `${cellSize}px`;
 
+    // Add the child div to the grid HTML
     wrapper.appendChild(newDiv);
   }
 
@@ -55,4 +62,36 @@ function createGrid(val) {
   makeSketch();
 }
 
-createGrid(16); // Default state
+// Change Grid function
+let changeGridSize = () => {
+  // Ask for a number and check if it's between acceptable parameters
+  let input = prompt("Enter your grid size:");
+  while (input < 1 || input > 100) {
+    input = prompt("The grid must be between 1 and 100 squares wide");
+  }
+  // Looks for a non numeric character
+  let regex = /\D|\W/;
+  
+  // If the test returns false, i.e., you entered an integer
+  while (regex.test(input)) {
+    alert("You entered a non numeric character.");
+    return;
+  }
+  
+  createGrid(input);
+};
+
+// Makes cells change color when hovering
+function makeSketch() {
+  wrapper.childNodes.forEach((cell) =>
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = `${selectColor()}`;
+    })
+  );
+}
+
+// Button Mappings
+changeGridBtn.addEventListener("click", changeGridSize);
+clearBtn.addEventListener("click", clearGrid);
+
+createGrid(16); // Default state, gets executed when site is loaded
